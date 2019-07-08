119b1ae2113ea0c43f4f8387917c5cc657b3c10c
cosminalco <cosmin.a.c@outlook.com>
2019-07-08 14:26:25 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-dexie-appdemo-bxcxza
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dexie-appdemo-bxcxza)
---
+++ console.ts
@@ -0,0 +1,13 @@
+export class Console {
+    textarea: HTMLTextAreaElement;
+    constructor() {
+        this.textarea = document.createElement('textarea');
+    }
+    log(txt: string, type?: string) {
+        if (type) this.textarea.value += type + " ";
+        this.textarea.value += txt + "\n";
+    }
+    error = function(txt: string) {
+        this.log(txt, "ERROR!");
+    }
+}
